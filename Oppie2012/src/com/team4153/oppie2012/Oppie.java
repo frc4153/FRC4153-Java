31648223f713e5707b0c4a19babef0be29276231
ergouser <jredman@ergotech.com>
2012-06-29 14:11:18 -0600

    Code for testing.

--- Oppie2012/src/com/team4153/oppie2012/Oppie.java
+++ Oppie2012/src/com/team4153/oppie2012/Oppie.java
@@ -4,11 +4,13 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package com.team4153.oppie2012;
 
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,17 +20,49 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class Oppie extends SimpleRobot {
+
+    public static final int LEFT_FRONT_MOTOR = -1;
+    public static final int LEFT_REAR_MOTOR = -1;
+    public static final int RIGHT_FRONT_MOTOR = -1;
+    public static final int RIGHT_REAR_MOTOR = -1;
+    public static final int LEFT_JOYSTICK = 1;
+    public static final int RIGHT_JOYSTICK = 2;
+    public static final int MANIPULATOR_JOYSTICK = 2;
+    RobotDrive drive = new RobotDrive(LEFT_FRONT_MOTOR, LEFT_REAR_MOTOR, RIGHT_FRONT_MOTOR, RIGHT_REAR_MOTOR);
+    Joystick leftStick = new Joystick(LEFT_JOYSTICK);
+    Joystick rightStick = new Joystick(RIGHT_JOYSTICK);
+    Joystick manipulatorStick = new Joystick(MANIPULATOR_JOYSTICK);
+
+    /**
+     * Create the state model for the shooter.
+     */
+    protected void robotInit() {
+        super.robotInit();
+        // oops, wait, it's a singleton, don't need to do this.
+    }
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
+        while (isOperatorControl() && isEnabled()) {
+            try {
+                //drive.tankDrive(leftStick, rightStick);  // no driving yet.
+                StateModel.getStateModel().runRoller(manipulatorStick.getRawButton(3));
+                StateModel.getStateModel().runBelt(manipulatorStick.getRawButton(4));
+                StateModel.getStateModel().runFeed(manipulatorStick.getRawButton(5));
+                StateModel.getStateModel().shoot(manipulatorStick.getRawButton(1));
+                Timer.delay(0.005);
+            } catch (CANTimeoutException ex) {
+                ex.printStackTrace();
+            }
+        }
 
     }
 }
